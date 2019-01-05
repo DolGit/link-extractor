7b20985563884cde474af34ff7597373f439f638
Alex Dollery <alexdollery@yahoo.co.uk>
2019-01-05 13:23:19 +0300

    added a links store for re-finding urls

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
+const links = {}
+
+const find = (el, selector) => {
+    if (el) return false
+    if (links[selector]) return links[selector]
+}
+
 const extract = (el) => {
     if (!el || !el.attributes) return false
     const obj = {}
@@ -8,19 +15,23 @@ const extract = (el) => {
 
 export function extractLink(selector) {
     const el = document.querySelector(selector)
+    if (find(el, selector)) return find(el, selector)
     if (!el) return false
     const extracted = extract(el)
+    links[selector] = extracted
     el.parentNode.removeChild(el)
     return extracted
 }
 
 export function extractLinks(selector) {
     const el = document.querySelector(selector)
+    if (find(el, selector)) return find(el, selector)
     if (!el) return false
     const links = [].slice.call(el.querySelectorAll('a'), 0);
     const extracted = links.map(child => {
         return extract(child);
     })
+    links[selector] = extracted
     el.parentNode.removeChild(el)
     return extracted
 }
